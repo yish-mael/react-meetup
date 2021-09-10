4d70a10b9258ea314c3d9ed36514bf9abbdd83ff
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2021-09-10 21:45:31 +0100

    feat: add route to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetUpsPage from './pages/AllMeetUps';
+import NewMeetUpPage from './pages/NewMeetUp';
+import FavoritesPage from './pages/Favorites';
+
 function App() {
   return (
     <div>
-      
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetUpsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetUpPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
