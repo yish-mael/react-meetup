c47497555e7435c1a9c01807b785a2ae33a35e3c
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2021-09-10 22:21:06 +0100

    feat: add MainNavigation component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetUpsPage from './pages/AllMeetUps';
 import NewMeetUpPage from './pages/NewMeetUp';
 import FavoritesPage from './pages/Favorites';
-
+import MainNavigation from './components/layout/MainNavigation';
 function App() {
   return (
     <div>
+      <MainNavigation />
+      
       <Switch>
         <Route path='/' exact>
           <AllMeetUpsPage />
