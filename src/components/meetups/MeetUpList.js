3ed0166c9772d43284c85b3a55c434ffc574d307
Tolu Akinnubi <tolu.akinnubi@yahoo.com>
2021-09-11 19:34:26 +0100

    feat: add Card component

--- src/components/meetups/MeetUpList.js
+++ src/components/meetups/MeetUpList.js
@@ -1,19 +1,25 @@
 import classes from './MeetUpList.module.scss';
 import MeetUpItem from './MeetUpItem';
+import Card from '../ui/Card';
 
 function MeetUpList(props) {
     return (
-        <ul className={classes.list}>
-            {props.meetups.map((meetup) => (
-            <MeetUpItem
-            key={meetup.id} 
-            id={meetup.id} 
-            image={meetup.image} 
-            title={meetup.title} 
-            address={meetup.address}
-            description={meetup.description}
-             />))}
-        </ul>
+        <div>
+            <Card>
+                <ul className={classes.list}>
+                    {props.meetups.map((meetup) => (
+                    <MeetUpItem
+                    key={meetup.id} 
+                    id={meetup.id} 
+                    image={meetup.image} 
+                    title={meetup.title} 
+                    address={meetup.address}
+                    description={meetup.description}
+                    />))}
+                </ul>
+            </Card>
+        </div>
+        
     );
 }
 
